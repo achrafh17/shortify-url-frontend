6e7b8fbe913f30b53c4fc99d710d80651e420675
Achraf <achrafhafid565@gmail.com>
2025-03-06 21:49:51 +0100

    fisrt commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import react from "react";
+import { useState, useRef } from "react";
+function App() {
+  const [inputurl, setinputurl] = useState("");
+  const [shorturl, setShorturl] = useState("");
+  const inputref = useRef();
+  const handlleshorturl = () => {
+    fetch("http://localhost:3000/app", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url: inputurl }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setShorturl(data.shorturl);
+      })
+      .catch((err) => console.log(err));
+    console.log(inputurl);
+  };
+  const copier=()=>{
+    navigator.clipboard.writeText(shorturl)
+ 
+  }
+  return (
+    <div className="App">
+      <div className="main">
+        <h1>shortify</h1>
+        <p>Transform long URLs into compact aliases</p>
+        <div className="input-button">
+          <input onChange={(e) => setinputurl(e.target.value)} type="text" />
+          <button onClick={handlleshorturl}>Shorten url</button>
+          <h1>your shorten url is {shorturl}</h1>
+          <button onClick={copier}> copier</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
